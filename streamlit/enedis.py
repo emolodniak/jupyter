1bc43d235346262e6f16c4738b039cf156f97b17
emolodniak <108135026+emolodniak@users.noreply.github.com>
2023-06-21 14:26:12 +0200

    0

--- streamlit/enedis.py
+++ streamlit/enedis.py
@@ -2,42 +2,58 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import requests
+import altair as alt
 
-#Setup
+#setup
 st.set_page_config(
     page_title="U-Need Enedis Dashboard",
     page_icon="https://d3v4jsc54141g1.cloudfront.net/uploads/mentor/avatar/1575105/normal_Enedis_Icone_couleur_RVB_300_dpi-1552312893.png",
-    layout="wide",
+    layout="wide"
 )
+#backgroundColor="#FFFFFF"
 
 #Sidebar
+#front logo https://data.enedis.fr/assets/logo?tstamp=16396808858804536
+dic = pd.read_csv('C:\\Users\\Invite\\Documents\\Python\\streamlit\\DictionnaireProfils_4JUIL20.csv', encoding='cp1252', sep=';')
+
 st.sidebar.title("Filtres")
 with st.sidebar:
-  dataset = st.sidebar.selectbox("Dataset",
-      ('bilan-electrique', 'coefficients-des-profils')
+  dataset = st.sidebar.selectbox("DATASET",
+      (['coefficients-des-profils'])
   )
   category = st.sidebar.selectbox("CATEGORIE",
-      ('Residentiel', 'Professionnel')
+      (dic['Catégorie'].unique())
+      #('Residentiel', 'Professionnel')
   )
   profile = st.sidebar.selectbox("SOUS_PROFIL",
-      ('RES5_HPSH','RES5_HCSH','RES5_HPSB','RES5_HCSB','PRO1_BASE','PRO1WE_SEM','PRO1WE_WE','PRO2_HP','PRO2_HC')
+      dic['Sous-profil'][dic['Catégorie']==category]
+      #('RES5_HPSH','RES5_HCSH','RES5_HPSB','RES5_HCSB','PRO1_BASE','PRO1WE_SEM','PRO1WE_WE','PRO2_HP','PRO2_HC')
   )
   year = st.radio("HORODATE",
-        ('2019', '2020', '2021', '2022', '2023')
+        ('2020', '2021', '2022', '2023'), horizontal=True
     )
 
-def get_data():
+#@st.cache_data
+
+def url():
   url = f'https://data.enedis.fr/api/records/1.0/search/?dataset=coefficients-des-profils&q=&rows=9999&facet=horodate&facet=sous_profil&facet=categorie&refine.categorie={category}&refine.sous_profil={profile}&refine.horodate={year}'
+  #url = 'https://data.enedis.fr/api/records/1.0/search/?dataset=trajectoire-du-nombre-de-points-de-charge-accessibles-au-public&rows=1000&sort=annee'
+  return url
+
+url = url()
+
+
+
+def get_data():
   df = pd.json_normalize(requests.get(url).json()['records'])
   df.drop(columns=['datasetid','recordid','record_timestamp'], inplace=True)
   df.columns = df.columns.str.replace('fields.', '')
-  #df.sort_values('horodate', ignore_index=True, inplace=True)
+  df['horodate'] = pd.to_datetime(df['horodate'], format='%Y-%m-%dT%H:%M:%S.%f')
+  df.sort_values('horodate', ignore_index=True, inplace=True)
   return df
 
 with st.sidebar:
   with st.spinner("Loading..."):
-    #@st.cache_data
-    # Data
     data = get_data()
 
 st.title(':blue[ENEDIS OPEN DATA]')
@@ -48,24 +64,38 @@ css = '''
     .stTabs [data-baseweb="tab-list"] button [data-testid="stMarkdownContainer"] p {font-size:18px; color:DodgerBlue;}
 </style>
 '''
+##  padding-right:0rem; padding-bottom:1rem; background:lightgrey; 
 st.markdown(css, unsafe_allow_html=True)
 
 tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['🛈 Informations', ':bookmark_tabs: Tableau', ':bar_chart: Analyse', 'ℹ En savoir plus', ':inbox_tray: Export', ':gear: API'])
 
 with tab1:
-  r = requests.get('https://data.enedis.fr/explore/dataset/coefficients-des-profils/information/')
+  r = requests.get(f'https://data.enedis.fr/explore/dataset/{dataset}/information/')
   st.markdown(r.text.split('content="')[4].split('"')[0])
-  st.divider()
 
+#tab2.subheader("A tab with the data")
+#tab2.write(data.set_index('annee').T)
 tab2.write(data)
 st.divider()
 
 with tab3:
+  st.subheader(''.join(dic['Libellé'][dic['Sous-profil']==profile]))
+  period = st.radio("PERIOD",
+        ('Mois', 'Jour', 'Heure'), horizontal=True
+    )
+  DF = data
+  DF['horodate'] = pd.to_datetime(DF['horodate'], format='%Y-%m')
+  if period == 'Mois':
+    DF = DF.set_index('horodate').resample('M').mean().reset_index()
+  if period == 'Jour':
+    DF = DF.set_index('horodate').resample('D').mean().reset_index()
+  if period == 'Heure':
+    DF = DF.set_index('horodate').resample('H').mean().reset_index()
   variables = st.multiselect(
-      "Selectionnez des variables", data.select_dtypes('float').columns, data.select_dtypes('float').columns.values
+      "VARIABLES", 
+      DF.select_dtypes('float').columns.values, DF.select_dtypes('float').columns.values[0]
   )
-  st.line_chart(data=data, x='horodate', y=variables) #['coefficient_ajuste','coefficient_prepare'])
-  st.divider()
+  st.line_chart(data=DF, x='horodate', y=variables) #['coefficient_ajuste','coefficient_prepare'])
 
 
 with tab5:
@@ -75,4 +105,6 @@ with tab5:
     file_name=f'{dataset}_{category}_{profile}_{year}.csv',
     mime='text/csv')
   st.write('Le CSV utilise le point-virgule (;) comme séparateur.)')
-  st.divider()
+
+with tab6:
+  st.markdown(f'[ :link: API GET request]({url})')
